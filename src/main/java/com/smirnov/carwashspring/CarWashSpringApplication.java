245950f3f3a4ff7b07dd4b617893638a71c04065
Dmitry <smirnoff.spb78@yandex.ru>
2024-06-08 21:10:24 +0300

    Initial commit

---
+++ src/main/java/com/smirnov/carwashspring/CarWashSpringApplication.java
@@ -0,0 +1,13 @@
+package com.smirnov.carwashspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarWashSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smirnov/carwashspring/CarWashSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smirnov.carwashspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
